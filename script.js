343d23877e35de3b4330a545b417464d5b6dd52c
NirnayBhattacharya <bnirnay710@gmail.com>
2024-08-04 22:40:26 +0000

    code

---
+++ script.js
@@ -0,0 +1,9 @@
+var word = "airplane";
+var number = 16; 
+var calculatedNumber = number * 2; 
+var boolean = true; 
+var userInput = prompt("Dream vacay?");
+var fullstory = "It was summer break, and you decided to go on an " + word + " to " + userInput + ". " +
+"You were turning " + number + " and the airplane was " + calculatedNumberNumber + " years old. " +
+"So you realized that it's " + boolean + "that airplanes are so old!";
+console.log(fullstory);
